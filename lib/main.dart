0d4cdc35f9f5fd56a6af9b1d5efd27fa26468595
Shuanglin <2447749681@qq.com>
2020-08-02 19:58:07 +0800

    list widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,72 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-//void main() {
-//  runApp(MyApp());
-//}
-
-main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      title: 'Hello Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            'Profile',
-          ),
+          title: Text("Product List"),
         ),
-        body: ContentWidget(),
+        body: HomeContent(),
       ),
     );
   }
 }
 
-class ContentWidget extends StatelessWidget {
+class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextWidget(),
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: ListView(
+        children: <Widget>[
+          ProductItem('Product1', 'Macbook pro1',
+              'https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg'),
+          ProductItem('Product2', 'Macbook pro2',
+              'https://tva1.sinaimg.cn/large/006y8mN6gy1g72imm9u5zj30u00k0adf.jpg'),
+          ProductItem('Product3', 'Macbook pro3',
+              'https://tva1.sinaimg.cn/large/006y8mN6gy1g72imqlouhj30u00k00v0.jpg'),
+        ],
+      ),
     );
   }
 }
 
-class TextWidget extends StatelessWidget {
+class ProductItem extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String pictureUrl;
+
+  ProductItem(this.title, this.subtitle, this.pictureUrl);
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'hello world',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(fontSize: 30, color: Colors.cyan),
+    return Container(
+      decoration: BoxDecoration(border: Border.all(width: 2)),
+      padding: EdgeInsets.all(4),
+      child: Column(
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontSize: 24),
+          ),
+          Text(
+            subtitle,
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Image.network(pictureUrl),
+        ],
+      ),
     );
   }
 }
